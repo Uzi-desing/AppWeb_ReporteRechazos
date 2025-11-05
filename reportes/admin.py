420f3df35e7b697d0da9c8cd6f2cc0e9fa1a63bd
Uzi-desing <Uzielcares@outlook.com>
2025-11-05 03:50:03 -0300

    Registro de los models para el administrador

--- reportes/admin.py
+++ reportes/admin.py
@@ -1,13 +1,16 @@
 from django.contrib import admin
-from .models import Cliente, Obras, UsuarioTransportista, ReporteDano, Piezas, Disponibilidad, Categoria, CategoriaDano, PiezaRechazada
+from .models import Categoria, Disponibilidad, CategoriaDano, Rol, Empleado, Cliente, Obras, UsuarioTransportista, Piezas, ReporteDano, PiezaRechazada
+
 # Register your models here.
 
-admin.site.register(Obras)
-admin.site.register(Cliente)
-admin.site.register(UsuarioTransportista)
-admin.site.register(ReporteDano)
-admin.site.register(Piezas)
-admin.site.register(Disponibilidad)
 admin.site.register(Categoria)
+admin.site.register(Disponibilidad)
 admin.site.register(CategoriaDano)
+admin.site.register(Rol)
+admin.site.register(Empleado)
+admin.site.register(Cliente)
+admin.site.register(Obras)
+admin.site.register(UsuarioTransportista)
+admin.site.register(Piezas)
+admin.site.register(ReporteDano)
 admin.site.register(PiezaRechazada)
